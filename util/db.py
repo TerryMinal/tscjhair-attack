68a674632b81076961752e00cc64433038ef27c6
darthbeep <darthbeep@gmail.com>
2018-01-12 00:18:59 -0500

    fixed database a little

--- util/db.py
+++ util/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from hashlib import sha1
 #Databases:
     #images: name|id|url|tags
     #users: username|email|password|ml
@@ -12,11 +12,16 @@ def command(command, f='data.db'):
     db.close()
     return r
 
+def return_command(command, f='data.db'):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    r = c.execute(command)
+    return r
+
 def print_all_from_table(table, f='data.db'):
     db = sqlite3.connect(f)
     c = db.cursor()
     inp = "SELECT * FROM " + table + ";"
-    print inp
     r = c.execute(inp)
     for entry in r:
         print entry
@@ -36,7 +41,8 @@ def get_table_size(table, f='data.db'):
 
 
 def add_person(username, email, password, ml):
-    comm = "INSERT INTO users VALUES (\"" + str(get_table_size('users')) + "\", \"" + username + "\", \"" + email + "\", \"" + password + "\", \"" + ml + "\");"
+    tempp = sha1(password).hexdigest()
+    comm = "INSERT INTO users VALUES (\"" + str(get_table_size('users')) + "\", \"" + username + "\", \"" + email + "\", \"" + tempp + "\", \"" + ml + "\");"
     command(comm)
 
 def get_ml_by_id(id, f='data.db'):
@@ -52,5 +58,17 @@ def get_ml_by_id(id, f='data.db'):
 def edit_ml_by_id(id, ml, f='data.db'):
     comm = "UPDATE users SET ml=\"" + ml + "\" WHERE id=\""+str(id)+"\";"
     command(comm)
+
+def get_authentication(username, password, f='data.db'):
+    comm = "SELECT * FROM users WHERE username=\""+username+"\";"
+    user = return_command(comm)
+    for use in user:
+        if use[3] == sha1(password).hexdigest():
+            return True
+    return False
+        #for us in use:
+        #    print us
+    #if password==use
+
 
 print_all_from_table('users')
