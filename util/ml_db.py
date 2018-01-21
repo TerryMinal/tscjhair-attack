d75a8950b6e581329e5957de9940ad12e433a419
Jennifer Zhang <jennifer.newyork@aol.com>
2018-01-21 15:22:04 -0500

    added ml_db.py

---
+++ util/ml_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def init_db(f='util/ml_db.db'):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute("CREATE TABLE ml(name TEXT PRIMARY KEY, val INTEGER)")
+    db.commit()
+    db.close()
+
+def add_to_table(name, f='util/ml_db.db'):
+    db=sqlite3.connect(f)
+    c=db.cursor()
+    c.execute("SELECT val FROM ml WHERE val = (SELECT MAX(val) FROM ml)")
+    prev_val = c.fetchall()
+    if len(prev_val) == 0:
+        next_val = 0
+    else:
+        next_val = prev_val[0][0] + 1
+    c.execute("INSERT INTO ml VALUES('%s', '%d'" %(name, next_val))
+        
+    db.commit()
+    db.close()
