21552bc3171dd39b24815eb06294bec93c0a0a97
TerryMinal <tguan@stuy.edu>
2018-01-21 18:06:58 -0500

    added find feature in ml.db

--- util/ml_db.py
+++ util/ml_db.py
@@ -12,11 +12,33 @@ def add_to_table(name, f='util/ml_db.db'):
     c=db.cursor()
     c.execute("SELECT val FROM ml WHERE val = (SELECT MAX(val) FROM ml)")
     prev_val = c.fetchall()
+    print prev_val
     if len(prev_val) == 0:
         next_val = 0
     else:
         next_val = prev_val[0][0] + 1
     c.execute("INSERT INTO ml VALUES('%s', '%d')" %(name, next_val))
-        
+
     db.commit()
     db.close()
+
+# finds the word inside the db
+# if it exists, it'll return its value
+# else return -1
+def find_word(name, f='util/ml_db.db'):
+    db = sqlite3.connect(f)
+    c=db.cursor()
+    c.execute("SELECT name, val FROM ml WHERE name = '%s' " %name)
+    val = c.fetchall()
+    db.commit()
+    db.close()
+    if len(val) == 0:
+        return -1
+    else:
+        return val[0][1]
+
+
+# init_db('ml_db.db')
+# add_to_table("pew", "./ml_db.db")
+print find_word("cool", './ml_db.db')
+print find_word("dne", './ml_db.db')
--- util/spotify.py
+++ util/spotify.py
@@ -1,5 +1,5 @@
 import spotipy
-token = ""
+token = "BQCIC3mVpCgwW7bt4iCLDjYOOyYMS6B5wCG8-4KWSXKPv7N1cTqsusgDaYTOhyAQNYZJziODTxR4SG1FC6O1r9BWouipluGz0rrtCh_Suoz6m-DKdM0L-0IxUx_OLSkX6zf_-fUnwavCpiS1Kx4AavVCcY-GvFdaUxkAPQv5CdtEw4jFb6EZ0O00PQ"
 def get_tags_from_artist(artist):
     spotify = spotipy.Spotify(auth=token)
     results = spotify.search(q='artist:' + artist, type='artist')
