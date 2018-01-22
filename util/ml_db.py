6907716b36180a1a70ad0b847e6a6e47c61683a5
TerryMinal <tguan@stuy.edu>
2018-01-21 21:58:53 -0500

    adding rand_val function

--- util/ml_db.py
+++ util/ml_db.py
@@ -40,22 +40,24 @@ def find_word(content, thing, f='util/ml_db.db'):
     else:
         return val[0][1]
 
-def random_val( f='util/ml_db.db'):
+def random_val(content, f='util/ml_db.db'):
     db = sqlite3.connect(f)
     c=db.cursor()
-    c.execute("SELECT val FROM ml WHERE val = (SELECT MAX(val) FROM ml)")
-    max_val = c.fetchall() + 1
-    if len(max_val) == 0:
+    c.execute("SELECT val FROM %s WHERE val = (SELECT MAX(val) FROM %s)" %(content, content)) # used to get the max val
+    max_val = c.fetchall()
+    if len(max_val) == 0: #in the case where there is nothing in the table
         return -1
-    i = random() * max_val
-    c.execute("SELECT name, val FROM ml WHERE val = '%d' " %i)
+    print "max: ", max_val[0][0] + 1
+    i = random() * (max_val[0][0] + 1)
+    c.execute("SELECT %s, val FROM %s WHERE val = '%d' " %(content, content, i)) # gets a random name and value pair
+    val = c.fetchall()
     db.commit()
     db.close()
-    val = c.fetchall()
     return val[0][1]
 
 
 # init_db('ml_db.db')
-# add_to_table("genre", "pew", "./ml_db.db")
+# add_to_table("genre", "wart", "./ml_db.db")
 print find_word("genre", "cool", './ml_db.db')
 print find_word("genre", "pew", './ml_db.db')
+print random_val("genre", './ml_db.db')
